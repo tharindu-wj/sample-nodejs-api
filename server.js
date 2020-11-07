2d50b88fd9d1763b2659db0c91d234c456207fcb
tharinduwjh <tharinduwjh@gmail.com>
2020-11-07 22:48:04 +0530

    morgan added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Routes
 const { bootcamps } = require('./routes');
@@ -8,6 +9,11 @@ const { bootcamps } = require('./routes');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
