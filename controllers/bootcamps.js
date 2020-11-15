4f0a47a15ffe501caca6662dfe87050a33ff8eb9
tharindu.wick <tharindu.wickramasinghe-ext@harver.com>
2020-11-15 18:25:11 +0530

    error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const { Bootcamp } = require('../models');
+const ErrorResponse = require('../utils/ErrorResponse');
 
 /**
  * @desc Get all
@@ -25,11 +26,13 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      return res.status(404).json({ success: false, data: null });
+      return next(
+        new ErrorResponse(`Bootcamp not found for id: ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(500).json({ success: false });
+    next(new ErrorResponse(`Bootcamp not found for id: ${req.params.id}`, 404));
   }
 };
 
--- server.js
+++ server.js
@@ -13,10 +13,13 @@ connectDB();
 // Routes
 const { bootcamps } = require('./routes');
 
+// Middleware
+const errorHandler = require('./middlewares/error');
+
 const app = express();
 
 // Body parser
-app.use(express.json())
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -26,11 +29,16 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// Error handling
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promises
