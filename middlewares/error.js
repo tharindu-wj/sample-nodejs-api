4f2863082b82c4828006d30884f7debdcafc3f6c
tharindu.wick <tharindu.wickramasinghe-ext@harver.com>
2020-11-15 19:27:25 +0530

    sync handler

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,10 +1,28 @@
+const ErrorResponse = require('../utils/ErrorResponse');
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err, message: err.message };
   // log error stack
-  console.log(err.stack.red);
+  console.log(error);
+
+  // Mongoose bad objectId
+  if (err.name === 'CastError') {
+    error = new ErrorResponse(`Bootcamp not found for id: ${err.value}`, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    error = new ErrorResponse('Duplicate field value entered', 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorResponse(message, 400);
+  }
 
   res
-    .status(err.statusCode || 500)
-    .json({ success: false, error: err.message || 'Server Error' });
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || 'Server Error' });
 };
 
 module.exports = errorHandler;
